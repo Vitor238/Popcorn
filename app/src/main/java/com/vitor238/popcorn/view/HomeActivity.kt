a86b89a40ccd2b8cf42bb4d82e7a18ef91987042
Vitor Ferreira <vitorfer238@gmail.com>
2020-11-17 20:23:49 -0300

    Profile picture on the toolbar on the settings screen. Username displayed on the settings screen.

--- app/src/main/java/com/vitor238/popcorn/view/HomeActivity.kt
+++ app/src/main/java/com/vitor238/popcorn/view/HomeActivity.kt
@@ -1,10 +1,15 @@
 package com.vitor238.popcorn.view
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.google.firebase.auth.FirebaseAuth
 import com.vitor238.popcorn.R
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -21,9 +26,31 @@ class HomeActivity : BaseActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            val settingsItem = menu?.findItem(R.id.menu_settings)
+            Glide.with(this).asDrawable().load(currentUser.photoUrl)
+                .circleCrop()
+                .into(object : CustomTarget<Drawable>() {
+                    override fun onResourceReady(
+                        resource: Drawable,
+                        transition: Transition<in Drawable>?
+                    ) {
+                        settingsItem?.icon = resource
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {
+
+                    }
+                })
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.menu_profile -> {
+            R.id.menu_settings -> {
                 val intent = Intent(this, SettingsActivity::class.java)
                 startActivity(intent)
             }
--- app/src/main/java/com/vitor238/popcorn/view/SettingsActivity.kt
+++ app/src/main/java/com/vitor238/popcorn/view/SettingsActivity.kt
@@ -3,6 +3,7 @@ package com.vitor238.popcorn.view
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.vitor238.popcorn.R
 import com.vitor238.popcorn.viewmodel.LoggedInViewModel
 import com.vitor238.popcorn.viewmodel.LoggedInViewModelFactory
@@ -26,7 +27,8 @@ class SettingsActivity : BaseActivity() {
 
         loggedInViewModel.userMutableLiveData.observe(this) {
             if (it != null) {
-                text_email.text = it.email
+                text_email.text = it.displayName
+                Glide.with(this).load(it.photoUrl).circleCrop().into(image_profile)
             }
         }
     }
